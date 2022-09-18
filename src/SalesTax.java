48304445bc4d01f289d13d9de75e617de28b15f1
dougaler <dougaler@mail.uc.edu>
2022-09-18 12:55:18 -0400

    Completed Sales Tax program

---
+++ src/SalesTax.java
@@ -0,0 +1,11 @@
+public class SalesTax {
+    public static void main(String[] args) {
+        double InitialPrice = 23.5;
+        double tax;
+
+        tax = InitialPrice * 0.05;
+
+        System.out.println("The 5 percent tax of "  + InitialPrice + " is: " + tax);
+
+    }
+}
